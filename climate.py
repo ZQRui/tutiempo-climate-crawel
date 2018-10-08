0efd44ad9b2fb5873e9fd2b9eaceca06cdd2ce48
imakar <iMakar@qq.com>
2018-10-08 11:45:58 +0800

    修改日志格式

--- climate.py
+++ climate.py
@@ -16,12 +16,19 @@ def get_weather(year,month,place_id="548230"):
     logging.info("start get climate from place id: {place_id} year:{year} month:{month} ".format(place_id=place_id,year=year,month=month))
     url="https://en.tutiempo.net/climate/{month:02}-{year:04}/ws-{place_id}.html".format(year=year,month=month,place_id=place_id)
     result=se.get(url)
+    if result.status_code==404:
+        logging.error("place id error {}".format(place_id))
+        exit()
+        return
     b1=BeautifulSoup(result.content,"html.parser")
     form=b1.select("div.DivInfoAbuso form input")
     data=dict([(form[i].attrs["name"],form[i].attrs["value"]) for i in (0,1)])
     send_data="&".join(["{}={}".format(k,v) for k,v in data.items()])
     logging.debug("get taenc ok")
     result2=se.post(url,data=send_data)
+    if result2.status_code==404:
+        logging.error("climate data of: palce {} year {} month{} not exist".format(place_id,year,month))
+        return 
     b2=BeautifulSoup(result2.content,"html.parser")
     trs=b2.select(".mt5.minoverflow.tablancpy table tr")
     table=[[th.text for th in trs[0].select("th")],]+[[td.text.replace('\xa0','') for td in tr] for tr in trs[1:-2]]
@@ -31,7 +38,6 @@ def get_weather(year,month,place_id="548230"):
     data_dir="climate-data"
     if not os.path.isdir(data_dir):
         os.mkdir(data_dir)
-    
     fname="{place_id}-{year:04}-{month:02}.csv".format(year=year,month=month,place_id=place_id)
     fpath=os.path.join(data_dir,fname)
     with open(fpath,"w") as fp:
@@ -46,7 +52,10 @@ argp.add_argument("end_year",type=int,default=None)
 argp.add_argument("end_month",type=int,default=None)
 
 if __name__=="__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO,format = '%(asctime)s  %(filename)s : %(levelname)s  %(message)s', datefmt  = '%Y-%m-%d %A %H:%M:%S')
+    logging_file_handler=logging.FileHandler("climate-crawel.log", mode="a")
+    logging_file_handler.setLevel(logging.DEBUG)
+    logging.getLogger().addHandler(logging_file_handler)
     args=argp.parse_args()
     if not (args.end_year and args.end_month):
         get_weather(args.start_year,args.start_month,args.place_id)
